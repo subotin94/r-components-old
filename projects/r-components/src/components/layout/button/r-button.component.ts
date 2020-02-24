56a96634a80f94f1e1935c909f67c7b9934f9de7
Kukic Vladimir <kukic.vladimir@gmail.com>
2020-02-24 20:55:57 +0100

    parsing class name, input and output params

--- projects/r-components/src/components/layout/button/r-button.component.ts
+++ projects/r-components/src/components/layout/button/r-button.component.ts
@@ -32,8 +32,7 @@ import {
 export class RButtonComponent implements OnInit {
 
   @Input() size: RComponentSize = 'medium';
-  @Input()
-  status: RComponentStatus = 'primary';
+  @Input() status: RComponentStatus = 'primary';
   @Input() shape: RComponentShape = 'rectangle';
   @Input() appearance: RComponentAppearance = 'filled';
   icon: string;
